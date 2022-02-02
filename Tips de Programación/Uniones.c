508fd638c5aeae89c48330d71627bdf02a590003
Diego Suarez <1412612@utp.edu.pe>
2022-02-02 17:13:29 -0500

    agregar archivo

--- Tips de Programación/Uniones.c
+++ Tips de Programación/Uniones.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<stdint.h>
 
-typedef struct{
+typedef union{
     uint32_t a;
     char letras[4];
 }Datos;
@@ -9,7 +9,7 @@ typedef struct{
 int main(){
 
     Datos d;//declarando una estructura de tipo datos
-    d.a=10;
+    d.a=0;
     d.letras[0]='A';
     printf("El valor de a es: %d\n",d.a);
     printf("El valor de letras[0] es: %d\n",d.letras[0]);
